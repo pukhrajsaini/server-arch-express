2d3b2e76196e88196af3c8f824c2af5178fc214d
preeti9667 <saini04preeti@gmail.com>
2025-11-08 10:42:46 +0530

    Eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**', '*.js', '*.d.ts', 'assets/**', 'logs/**'],
+  }
+);
+
--- src/apis/v1/role/role.service.ts
+++ src/apis/v1/role/role.service.ts
@@ -1,48 +1,45 @@
-import { ServiceResponse } from "../../../interfaces";
-import { HttpStatus } from "../../../constants";
-import { ROLES } from "./role.constant";
-import RoleModel from "../../../models/role.model";
-import { LoggerUtil, OpenId } from "../../../utils";
-import adminService from "../admin/admin.service";
+import { ServiceResponse } from '../../../interfaces';
+import { HttpStatus } from '../../../constants';
+import { ROLES } from './role.constant';
+import RoleModel from '../../../models/role.model';
+import { LoggerUtil, OpenId } from '../../../utils';
+import adminService from '../admin/admin.service';
 
 class RoleService {
+  private logger = new LoggerUtil('RoleService');
 
-    private logger = new LoggerUtil('RoleService');
+  async createRole() {
+    for (const role of ROLES) {
+      const isExists = await RoleModel.findOne({ name: role.name });
+      if (isExists) {
+        await RoleModel.findByIdAndUpdate(isExists._id, {
+          name: role.name,
+          permissions: role.permissions,
+        });
 
-    async createRole() {
-        for (const role of ROLES) {
-
-            const isExists = await RoleModel.findOne({ name: role.name });
-            if (isExists) {
-                await RoleModel.findByIdAndUpdate(isExists._id, {
-                    name: role.name,
-                    permissions: role.permissions
-                });
-
-                this.logger.log("Role updated");
-            } else {
-                const displayId = OpenId.generate(6,'ROLE');
-                await RoleModel.create({
-                    displayId,
-                    name: role.name,
-                    permissions: role.permissions
-                });
-                this.logger.log("Role created");
-            }
-        }
-        await adminService.createAdmin();
+        this.logger.log('Role updated');
+      } else {
+        const displayId = OpenId.generate(6, 'ROLE');
+        await RoleModel.create({
+          displayId,
+          name: role.name,
+          permissions: role.permissions,
+        });
+        this.logger.log('Role created');
+      }
     }
+    await adminService.createAdmin();
+  }
 
-
-    async getRoles(): Promise<ServiceResponse> {
-        return {
-            status: HttpStatus.Ok,
-            message: 'roles_fetched',
-            data: {
-                roles: ROLES
-            }
-        }
-    }
+  async getRoles(): Promise<ServiceResponse> {
+    return {
+      status: HttpStatus.Ok,
+      message: 'roles_fetched',
+      data: {
+        roles: ROLES,
+      },
+    };
+  }
 }
 
 export default new RoleService();
